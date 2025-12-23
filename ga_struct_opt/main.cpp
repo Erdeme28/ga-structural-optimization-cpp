3c906acecda12ad665a5679f43be522272eddf20
Rotaru David <rotaru.davidmihai28@gmail.com>
2025-12-23 22:43:11 +0200

    Use TrussModel in main to build simple 2D truss

--- ga_struct_opt/main.cpp
+++ ga_struct_opt/main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include "model/TrussModel.h"
 
 int main() 
 {
-    std::cout << "Hello Genetic Algorithms!" << std::endl;
+    TrussModel model;
+
+	model.addNode(0.0, 1.0); // A
+	model.addNode(2.0, 1.0); // B
+	model.addNode(1.0, 0.0); // C
+
+	model.addBar(0, 1); // AB
+	model.addBar(0, 2); // AC
+	model.addBar(1, 2); // BC
+
+	std::cout << "Nodes:\n";
+	int index = 0;
+	for (const auto& node : model.getNodes())
+	{
+		std::cout << "Node " << index++ << " (" << node.x << ", " << node.y << ")\n";
+	}
+
+	std::cout << "\nBars:\n";
+	for (const auto& bar : model.getBars())
+	{
+		std::cout << "Bar from Node " << bar.startNode << " to Node " << bar.endNode << "\n";
+	}
+
     return 0;
 }
